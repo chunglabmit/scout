0ab1cb179edc1e76c82aad022c84ceb062e822f1
Justin Swaney <jswaney@mit.edu>
2019-08-17 12:08:05 -0400

    Update nuclei detection defaults

--- scout/main.py
+++ scout/main.py
@@ -102,9 +102,9 @@ scout preprocess histogram Ex_2_Em_2_stitched/ Ex2_hist.csv -s 50 -v
 scout preprocess rescale Ex_0_Em_0_stitched/ Ex0_hist.csv Ex0_rescaled -t 120 -p 99.7 -v
 scout preprocess rescale Ex_1_Em_1_stitched/ Ex1_hist.csv Ex1_rescaled -t 100 -p 99.7 -v
 scout preprocess rescale Ex_2_Em_2_stitched/ Ex2_hist.csv Ex2_rescaled -t 100 -p 99.7 -v
-scout preprocess convert Ex0_rescaled/ syto.zarr -v -n 4
-scout preprocess convert Ex1_rescaled/ sox2.zarr -v -n 4
-scout preprocess convert Ex2_rescaled/ tbr1.zarr -v -n 4
+scout preprocess convert Ex0_rescaled/ syto.zarr -v -n 8
+scout preprocess convert Ex1_rescaled/ sox2.zarr -v -n 8
+scout preprocess convert Ex2_rescaled/ tbr1.zarr -v -n 8
 
 scout nuclei detect syto.zarr nuclei_probability.zarr centroids.npy --voxel-size voxel_size.csv --output-um centroids_um.npy -v
 scout nuclei segment nuclei_probability.zarr centroids.npy nuclei_foreground.zarr nuclei_binary.zarr -v
